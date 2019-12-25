6aabd694bcc66775c6a4328fa653a83e39791252
conao3 <conao3@gmail.com>
2019-12-25 21:48:03 +0900

    tagged v1.0.0

--- ppp.el
+++ ppp.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2019  Naoya Yamashita
 
 ;; Author: Naoya Yamashita <conao3@gmail.com>
-;; Version: 0.0.1
+;; Version: 1.0.0
 ;; Keywords: tools
 ;; Package-Requires: ((emacs "26"))
 ;; License: AGPL-3.0

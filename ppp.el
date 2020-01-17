33e2a8ea63e2a9b96eca3f1b482d0df8e895c16d
conao3 <conao3@gmail.com>
2020-01-18 07:16:20 +0900

    refine readme, tagged v1.0.6

--- ppp.el
+++ ppp.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2019  Naoya Yamashita
 
 ;; Author: Naoya Yamashita <conao3@gmail.com>
-;; Version: 1.0.4
+;; Version: 1.0.6
 ;; Keywords: tools
 ;; Package-Requires: ((emacs "25.1"))
 ;; URL: https://github.com/conao3/ppp.el

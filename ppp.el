09e2357017bbea2f09254448bc8e8e45d7f65f8d
conao3 <conao3@gmail.com>
2020-01-20 02:00:54 +0900

    add condition-case indent rule

--- ppp.el
+++ ppp.el
@@ -174,7 +174,7 @@ See `ppp-plist' to get more info."
                  (while (not (eobp))
                    ;; (message "%06d" (- (point-max) (point)))
                    (let* ((sexp (sexp-at-point))
-                          (indent (or (and (memq sexp '(lambda)) 1)
+                          (indent (or (and (memq sexp '(lambda condition-case)) 1)
                                       (and (memq sexp '(closure)) 2)
                                       (ignore-errors
                                         (plist-get (symbol-plist sexp)

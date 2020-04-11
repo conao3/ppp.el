6651531da004b461ca7db233de91d4f896cd2cf4
conao3 <conao3@gmail.com>
2020-04-11 11:32:35 +0900

    add :mode-hook as :config like keyword

--- ppp.el
+++ ppp.el
@@ -309,11 +309,11 @@ Return t if scan succeeded and return nil if scan failed."
                   (ppp--insert "\n") (ppp--debug-ov-move)
                   (ppp--forward-sexp) (ppp--debug-ov-move)
                   (prog1 t
-                    (when (memq sexp '(:preface :init :config))
+                    (when (memq sexp '(:preface :init :config :mode-hook))
                       (ppp--insert "\n") (ppp--debug-ov-move)))))
                 (t
                  (cl-case key
-                   ((:preface :init :config)
+                   ((:preface :init :config :mode-hook)
                     (and
                      (ppp--add-newline-after-sexp 1) (ppp--debug-ov-move)))
                    (otherwise
